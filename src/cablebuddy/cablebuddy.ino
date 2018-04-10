e2fcf31fc1e988fe827cc277eed73a293ccf42b4
molnard <molnardavid84@gmail.com>
2018-04-10 14:39:10 +0200

    - added learn mode - learned cable stored in EEPROM - additional content (pictures and labels)

--- src/cablebuddy/cablebuddy.ino
+++ src/cablebuddy/cablebuddy.ino
@@ -1,4 +1,6 @@
 
+#include <EEPROM.h>
+
 typedef enum pinstate
 {
   PINSTATE_UNKNOWN = 0,
@@ -8,25 +10,58 @@ typedef enum pinstate
   PINSTATE_CONNECTED
 }a;
 
+
 #define lineSize 14
+
+
 
 typedef struct line
 {
   int pin; //like A0
-  pinstate state[lineSize];
+  pinstate state[lineSize]; //for every pin we will have a 'contact' array to other pins
 }b;
 
-#define PLED A2
-#define PBUTTON A4
+#define LED_GREEN A4
+#define LED_RED A3
+#define PBUTTON A5
+
+
+static line goodMatrix[lineSize];
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(PLED, OUTPUT);
-  digitalWrite(PLED, LOW);
-
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
   pinMode(PBUTTON, INPUT);
-  digitalWrite(PBUTTON, HIGH); //turn on pullup
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, HIGH);
+
+  goodMatrix[0].pin = 2;
+  goodMatrix[1].pin = 3;
+  goodMatrix[2].pin = 4;
+  goodMatrix[3].pin = 5;
+  goodMatrix[4].pin = 6;
+  goodMatrix[5].pin = 7;
+  goodMatrix[6].pin = 8;
+  goodMatrix[7].pin = 9;
+  goodMatrix[8].pin = 10;
+  goodMatrix[9].pin = 11;
+  goodMatrix[10].pin = 12;
+  goodMatrix[11].pin = A2;
+  goodMatrix[12].pin = A1;
+  goodMatrix[13].pin = A0;
+
+  int addr = 0;
+  for (int i=0;i<lineSize;i++) 
+  {    
+      for (int j=0;j<lineSize;j++) 
+      {
+        int value = EEPROM.read(addr);
+        goodMatrix[i].state[j] = (pinstate)value;
+        addr++;
+      }
+  }
 }
 
 static void generateConnectionMatrix(line * lines)
@@ -62,8 +97,15 @@ static void generateConnectionMatrix(line * lines)
 
 static unsigned int checkMatrices(line * good, line * current)
 {
-
-  
+  for (unsigned int i = 0; i < lineSize; i++)
+  {
+    for (unsigned int j = 0; j < lineSize; j++)
+    {
+      if (good[i].state[j] != current[i].state[j]) 
+        return i+1;
+    }
+  }
+  return 0;
 }
 
 static void printMatrix(line * lines)
@@ -82,44 +124,129 @@ static void printMatrix(line * lines)
     Serial.println();
   }
 }
-static line goodMatrix[lineSize];
+
+int result; /* the result of the previous cable check */
 
 void loop() {
   Serial.println("pincheck started");
+
+  /* initialize the matrix of the actual cable that will be measured to check */
   line lines[lineSize];
-  lines[0].pin = 2;
-  lines[1].pin = 3;
-  lines[2].pin = 4;
-  lines[3].pin = 5;
-  lines[4].pin = 6;
-  lines[5].pin = 7;
-  lines[6].pin = 8;
-  lines[7].pin = 9;
-  lines[8].pin = 10;
-  lines[9].pin = 11;
-  lines[10].pin = 12;
-  lines[11].pin = A2;
-  lines[12].pin = A1;
-  lines[13].pin = A0;
-
-  if (!digitalRead(PBUTTON))
+  for (int i=0;i<lineSize;i++)
   {
-    digitalWrite(PLED, HIGH);
-    while (!digitalRead(PBUTTON)); //wait for release
-
-    while (digitalRead(PBUTTON))
-    {
-      digitalWrite(PLED, !digitalRead(PLED));
-      delay(100);
-    }
-    digitalWrite(PLED, HIGH);
-    generateConnectionMatrix(goodMatrix);
-    digitalWrite(PLED, LOW);
+    lines[i].pin = goodMatrix[i].pin;
   }
-  
-  generateConnectionMatrix(lines);
-  printMatrix(lines);
 
-  delay(4000);  
+  int ledstate = 0;
+  Serial.println("waiting for button");
+  
+  /* IDLE state */
+  while (!digitalRead(PBUTTON)) 
+  {
+    if (result ==0) /* the cable is OK */
+    {
+      if (ledstate==0) digitalWrite(LED_GREEN, LOW);
+      else digitalWrite(LED_GREEN, HIGH);
+      ledstate++;
+      if (ledstate>1) ledstate = 0;
+    }
+    else /* the cable is NOT OK blinking the RED led [n] times. [n] the first wire which with BAD contact */
+    {
+        if (ledstate<result)
+        {
+          digitalWrite(LED_RED, LOW);
+          delay(100);
+          digitalWrite(LED_RED, HIGH);
+        }
+        ledstate++;
+        if ( (ledstate) > ( result + 5)) 
+          ledstate = 0;
+    }
+    delay(100);
+  }
+  int time = 0;
+  digitalWrite(LED_GREEN, LOW); 
+  /* check if the user wants to enter 'learn' mode */
+  while (digitalRead(PBUTTON))
+  {
+    delay(100);
+    time++;
+    if (time>100) /* 10 sec elapse d*/
+    {
+      /* blinking the LED to indicate 'learn' mode */
+      for (int i=0;i<10;i++)
+      {
+        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(LED_RED, HIGH);
+        delay(100);
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_GREEN, HIGH);
+        delay(100);
+      }
+      /* turn off LEDs */
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_RED, HIGH);
+
+      /* set up the matrix for the good condition */
+      generateConnectionMatrix(goodMatrix);
+
+      /* write to eeprom*/
+      int addr = 0;
+      for (int i=0;i<lineSize;i++) 
+      {    
+          for (int j=0;j<lineSize;j++) 
+          {
+            if (addr>EEPROM.length()) 
+            {
+              Serial.println("EEPROM full!");
+              while (1)
+              {
+                digitalWrite(LED_GREEN, HIGH);
+                digitalWrite(LED_RED, LOW);
+              }
+            }
+            EEPROM.write(addr, (int)goodMatrix[i].state[j]);
+            addr++;
+          }
+      }
+      
+      /*blinking the LED to indicate 'learn' mode finished*/
+      for (int i=0;i<10;i++)
+      {
+        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(LED_RED, LOW);
+        delay(100);
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_GREEN, HIGH);
+        delay(100);
+      }
+      break;
+    }
+  }
+
+  /* turn off LEDs */
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, HIGH);
+
+  /* set up the matrix for the current cable */
+  generateConnectionMatrix(lines);
+  
+  /* turn on LEDs */
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_RED, LOW);
+
+  /* for debug purposes*/
+  //Serial.println("good: ");
+  //printMatrix(goodMatrix);
+  //Serial.println("actual: ");
+  //printMatrix(lines);
+
+  /* compare the matrices */
+  result = checkMatrices(goodMatrix,lines);
+  Serial.print("result: ");
+  Serial.println(result);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, HIGH);
+  
 }
 
